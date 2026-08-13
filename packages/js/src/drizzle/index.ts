5bf0a734bd5f2663ca18d900e17d7e9b959ba2af
Kohki SHIKATA <kohki.shikata@gmail.com>
2026-08-13 20:47:59 +0900

    Clarify drizzle adapter stub is not yet implemented.

--- packages/js/src/drizzle/index.ts
+++ packages/js/src/drizzle/index.ts
@@ -2,6 +2,6 @@
  * Drizzle adapter for @b4moss/crudian.
  * Import via: `import { ... } from "@b4moss/crudian/drizzle"`
  *
- * Not implemented in v0.1.0.
+ * Not implemented yet (planned after bun-sqlite).
  */
 export {}
